c47aa68d02f27601c4a00a1042f1ba31b0535f2d
PencilBoxL <andrewliuzihua@gmail.com>
2018-10-02 15:43:20 +1000

    First Commit

---
+++ Instagram Project/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Instagram Project
+//
+//  Created by LiuYuHan on 2/10/18.
+//  Copyright © 2018 LiuYuHan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
